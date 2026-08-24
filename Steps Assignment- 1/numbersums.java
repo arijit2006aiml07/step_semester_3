8daef5c322a3f9824f0e8ff92432d4bd7808b725
arijit2006aiml07 <goswamiarijit2006@gmail.com>
2026-08-24 19:37:45 +0530

    Add files via upload

---
+++ Steps Assignment- 1/numbersums.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class NaturalSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter N: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+        int i = 1;
+
+        while (i <= n) {
+            sum = sum + i;
+            i++;
+        }
+
+        System.out.println("Sum of numbers from 1 to " + n + " = " + sum);
+    }
+}
