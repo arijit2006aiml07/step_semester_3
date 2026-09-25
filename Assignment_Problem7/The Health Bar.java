48605c9c9a5125bd8d896afba8cffe72f9d6fd02
Arijit Goswami <281015987+arijit2006aiml07@users.noreply.github.com>
2026-09-25 20:00:38 +0530

    Create The Health Bar.java

---
+++ Assignment_Problem7/The Health Bar.java
@@ -0,0 +1,27 @@
+public class Character {
+    private int health;
+    private final int maxHealth;
+
+    public Character(int maxHealth) {
+        this.maxHealth = maxHealth;
+        this.health = maxHealth;
+    }
+
+    public void takeDamage(int amount) {
+        this.health -= amount;
+        if (this.health < 0) {
+            this.health = 0;
+        }
+    }
+
+    public void heal(int amount) {
+        this.health += amount;
+        if (this.health > this.maxHealth) {
+            this.health = this.maxHealth;
+        }
+    }
+
+    public int getHealth() {
+        return this.health;
+    }
+}
